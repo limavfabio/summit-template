f8d81c15e384432fa7e28a103653f59a272cd78f
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-07-28 15:44:40 -0300

    Fix images on deployed website

--- js/main.js
+++ js/main.js
@@ -1,56 +1,56 @@
 // Variables
-const speakerCardWrapper = document.querySelector('.speaker-card-wrapper');
+const speakerCardWrapper = document.querySelector(".speaker-card-wrapper");
 // Data Storage - Array of objects for the speakers cards
 const speakers = [
   {
-    name: 'Curt Schilling',
-    title: 'Founder of 38 Studios',
+    name: "Curt Schilling",
+    title: "Founder of 38 Studios",
     description:
-      'After retiring, he founded Green Monster Games, which was renamed 38 Studios. The company released Kingdoms of Amalur: Reckoning in February 2012.',
-    image: 'checkered_business-hindi-man-1.png',
-    imageAlt: 'Yochai',
+      "After retiring, he founded Green Monster Games, which was renamed 38 Studios. The company released Kingdoms of Amalur: Reckoning in February 2012.",
+    image: "checkered_business-hindi-man-1.png",
+    imageAlt: "Yochai",
   },
   {
-    name: 'Jennifer MacLean',
-    title: 'Executive director of the IGDA',
+    name: "Jennifer MacLean",
+    title: "Executive director of the IGDA",
     description:
-      'After beginning her career working with Brian Reynolds and Sid Meier at Microprose Software in 1992, she joined AOL in 1996, where she became Programming Director for the Games Channel.',
-    image: 'Checkered_guestSpeaker.png',
-    imageAlt: 'Jennifer',
+      "After beginning her career working with Brian Reynolds and Sid Meier at Microprose Software in 1992, she joined AOL in 1996, where she became Programming Director for the Games Channel.",
+    image: "Checkered_guestSpeaker.png",
+    imageAlt: "Jennifer",
   },
   {
-    name: 'Yochai Benkler',
+    name: "Yochai Benkler",
     title:
-      'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
     description:
-      'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
-    image: 'checkered-blonde-man.png',
-    imageAlt: 'Yochai',
+      "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.",
+    image: "checkered-blonde-man.png",
+    imageAlt: "Yochai",
   },
   {
-    name: 'SohYeong Noh',
-    title: 'Director of Art Centre Nabi and a board member of CC Korea',
+    name: "SohYeong Noh",
+    title: "Director of Art Centre Nabi and a board member of CC Korea",
     description:
-      'As the Main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
-    image: 'checkered-business-woman-1.png',
-    imageAlt: 'SohYeong',
+      "As the Main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.",
+    image: "checkered-business-woman-1.png",
+    imageAlt: "SohYeong",
   },
   {
-    name: 'Todd McFarlane',
+    name: "Todd McFarlane",
     title:
-      'Canadian comic book creator, artist, writer, filmmaker and entrepreneur',
+      "Canadian comic book creator, artist, writer, filmmaker and entrepreneur",
     description:
       "In the late 1980s and early 1990s, McFarlane became a comic book superstar due to his work on Marvel Comics' Spider-Man franchise, on which he was the artist to draw the first full appearances of the character Venom.",
-    image: 'checkered-toddMcFar.png',
-    imageAlt: 'Todd',
+    image: "checkered-toddMcFar.png",
+    imageAlt: "Todd",
   },
   {
-    name: 'Yuzo Koshiro',
-    title: 'President of the video game development company Ancient.',
+    name: "Yuzo Koshiro",
+    title: "President of the video game development company Ancient.",
     description:
-      'Koshiro and his sister Ayano founded the game development company Ancient in 1990.',
-    image: 'Checkered_Yuzo-Koshiro.png',
-    imageAlt: 'Yuzo',
+      "Koshiro and his sister Ayano founded the game development company Ancient in 1990.",
+    image: "Checkered_Yuzo-Koshiro.png",
+    imageAlt: "Yuzo",
   },
 ];
 // Speakers cards generator
@@ -61,7 +61,7 @@ speakerCardWrapper.innerHTML = speakers
             <div class="d-flex">
               <img
                 class="me-3"
-                src="/images/speakers-pics/${card.image}"
+                src="images/speakers-pics/${card.image}"
                 alt="${card.imageAlt}"
               />
               <div class="ms-2">
@@ -74,6 +74,6 @@ speakerCardWrapper.innerHTML = speakers
                 </p>
               </div>
             </div>
-        `,
+        `
   )
-  .join('');
+  .join("");
